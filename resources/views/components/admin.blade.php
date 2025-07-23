af64a26a0feac54f9abd4797896eb7c05a71e2c2
genie <austinemathias9@gmail.com>
2025-07-23 12:40:55 +0100

    added the flowbite script cdn

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
-     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <style>
         .active{
             background: rgb(172, 171, 171);
@@ -15,18 +15,20 @@
 <body>
     <div>
        @include('components.layouts.admin.header')
-          @include('components.layouts.admin.sidebar')
-              <main>
-                <div class="p-4 pt-14 sm:ml-64 mt-10">
-                  {{ $slot }}
-                    
-                 </div>
-              </main>
+        @include('components.layouts.admin.sidebar')
+        <main>
+            <div class="p-4 pt-14 sm:ml-64 mt-10">
+                {{ $slot }}
+            
+            </div>
+        </main>
 
 
            </div>
         </div>
      </div>
+     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+
      <script data-navigate-once>
         document.addEventListener('livewire:navigated', () => {
             initFlowbite();
