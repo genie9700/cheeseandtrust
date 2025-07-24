3cb2cab66744f1863eaa813e59e950b7c5569934
genie <austinemathias9@gmail.com>
2025-07-24 17:43:44 +0100

    added logo to the auth pages

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -5,13 +5,11 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-4 sm:pt-0 bg-[#0E121B] dark:bg-gray-900">
-            {{-- <div>
-                <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div> --}}
+            <div class="mt-4">
+                <img width="140px" src="{{ asset('logo/chase2.png') }}" alt="">
+            </div>
 
-            <div class="w-full sm:max-w-md mt-4 px-6 py-4  dark:bg-gray-800  overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-4 px-6 my-4 py-4  dark:bg-gray-800  overflow-hidden sm:rounded-lg">
                 
                 {{ $slot }}
             </div>
